44105e55b8f1ccc5a4555119c743541ef3c32d09
sivaraj <sivaraj.r93@gmail.com>
2025-01-08 16:30:40 +0530

    Add files via upload

--- api/chat.js
+++ api/chat.js
@@ -1,5 +1,3 @@
-// /api/chat.js
-
 const { Server } = require("socket.io");
 
 export default (req, res) => {
@@ -7,20 +5,23 @@ export default (req, res) => {
     return res.status(200).send("OK");
   }
 
+  // Initialize Socket.io with the server
   const io = new Server(res.socket.server);
+
   io.on("connection", (socket) => {
     console.log("A user connected");
 
+    // Listen for chat messages
     socket.on("chat message", (msg) => {
-      io.emit("chat message", msg);
+      io.emit("chat message", msg); // Broadcast the message to all clients
     });
 
+    // Handle user disconnection
     socket.on("disconnect", () => {
       console.log("A user disconnected");
     });
   });
 
   res.socket.server.io = io; // Attach io instance to the server
-
   res.status(200).send("Socket.io server is running.");
 };
